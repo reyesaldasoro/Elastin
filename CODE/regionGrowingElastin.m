55247524de0df383b98851ba222a4f49dfb662d7
Constantino Carlos Reyes-Aldasoro <reyesaldasoro@googlemail.com>
2026-02-12 17:05:00 +0000

    Update regionGrowingElastin.m

--- CODE/regionGrowingElastin.m
+++ CODE/regionGrowingElastin.m
@@ -1,16 +1,23 @@
 function elastinLayers = regionGrowingElastin(watershedIntensity)
-
+%%
 maxIntensity        = max(watershedIntensity(:));
 seed                = watershedIntensity==maxIntensity;
-
-for k=1:700
-    if mod(k,50)==0
+%%
+for k=1:1999
+    if mod(k,50)==1
+        % Spur to remove branches that are going sideways   
         seed        = bwmorph(seed,'spur',5+floor(k/100));% 1+k/10);
         % seed_B1     = bwmorph(bwskel(seed),'branchpoints');
         % seed        = bwmorph(seed-seed_B1,'spur',5);% 1+k/10);
     end
     seed_E1         = bwmorph(seed,'endpoints');
-    seed_d          = imdilate(seed_E1,[1 1 1;1 1 1;1 1 1]);
+    if mod(k,200)==0
+        % dilation is normally ones(3) except every 100 to allow cases that have been broken by 1 pixel
+        seed_d          = imdilate(seed_E1,ones(7));
+        %seed_d          = imdilate(seed_E1,[0 1 1 1 0;1 1 1 1 1;1 1 1 1 1;1 1 1 1 1; 0 1 1 1 0]);
+    else
+        seed_d          = imdilate(seed_E1,[1 1 1;1 1 1;1 1 1]);
+    end
 
     %   seed_d      = imdilate(seed_d1,[0 1 0;1 1 1;0 1 0]);
     % curr        = (a8==k);
@@ -18,12 +25,14 @@ for k=1:700
     % curr_keep1  = unique(curr_L.*seed_d);
     % curr_keep   = ismember(curr_L,curr_keep1(2:end));
 
-     imagesc(2*seed+seed_d)
-     axis([3700 4300 3800 4700])  % fig 4
-    % axis([2100 2600 3100 3600])
-    %axis ([2100 2600 2700 3200])
-     drawnow
-     pause(0.0001)
+    if mod(k,10)==0
+        imagesc(2*seed+seed_d)
+        % axis([3700 4300 3800 4700])  % fig 4
+        axis([2100 2700 2600 3600])    % fig 1
+        %axis ([2100 2600 2700 3200])
+        drawnow
+        pause(0.0001)
+    end
      disp(k)
     n_seed = sum(sum(seed>0));
     seed            = seed|(seed_d.*(watershedIntensity>(250-floor(k/10))));
@@ -31,6 +40,11 @@ for k=1:700
 
     %disp([k -n_seed+n_seed2])
 end
+%% Once the main region growing is finished, join the layers, label and join one by one
+% Then spur, remove small holes and then find points touching Y junctions
+% and X junctions, H junctions
+
+
 
 %%
 % This may fill the lumen and everything else. It is necessary to check
