78dd1c9d747cde837867f1d0993f3ca9125c91ed
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2026-02-26 13:46:07 +0000

    Update labelElastinLayers.m

--- CODE/labelElastinLayers.m
+++ CODE/labelElastinLayers.m
@@ -13,7 +13,7 @@ outputLayersPoints(:,:,1)       = uint8(175*imdilate(elastinLayers,ones(3))) +..
 outputLayersPoints(:,:,2)       = uint8(240*imdilate(elastinEndPoints>0,ones(14))) +...
                                 (currImage(:,:,2).*uint8(1-imdilate(elastinEndPoints>0,ones(14)))) ;
 for k=1:numEndPoints
-    outputLayersPoints          = insertText(outputLayersPoints,[ elastinEndPoints_P(k).Centroid(1),elastinEndPoints_P(k).Centroid(2)],num2str(k),FontSize=50,BoxOpacity=0.2,TextColor='black',TextBoxColor='white');
+    outputLayersPoints          = insertText(outputLayersPoints,[ elastinEndPoints_P(k).Centroid(1),elastinEndPoints_P(k).Centroid(2)],num2str(k),FontSize=70,BoxOpacity=0.2,TextColor='black',TextBoxColor='white');
 end
 
 outputLayersPoints              = insertText(outputLayersPoints,[ 1,1],strcat('Number of points=',num2str(numEndPoints)),FontSize=200,BoxOpacity=0.2,TextColor='black',TextBoxColor='white');
