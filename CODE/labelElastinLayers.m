092682363b4fa376678ec8edd1a95acb34d8ce56
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2026-02-26 11:54:32 +0000

    Update labelElastinLayers.m
    coherent order of outputs

--- CODE/labelElastinLayers.m
+++ CODE/labelElastinLayers.m
@@ -1,19 +1,19 @@
-function  [OutputLayers, outputLayersPoints] = labelElastinLayers (currImage, elastinLayers,elastinEndPoints_3_L)
+function  [OutputLayers, outputLayersPoints] = labelElastinLayers (currImage, elastinLayers,elastinEndPoints)
 
-elastinEndPoints_3_P            = regionprops(elastinEndPoints_3_L,"Centroid");
-numEndpoints                    = max(elastinEndPoints_3_L(:));
+elastinEndPoints_P            = regionprops(elastinEndPoints,"Centroid");
+numEndPoints                    = max(elastinEndPoints(:));
 OutputLayers                    = currImage;
 outputLayersPoints              = currImage;
 OutputLayers(:,:,2)             = uint8(255*imdilate(elastinLayers,ones(3))) +...
                                 (currImage(:,:,2).*uint8(1-imdilate(elastinLayers,ones(3)))) ;
 
-outputLayersPoints(:,:,1)       = uint8(175*imdilate(elastinLayers_3,ones(3))) +...
-                                (currImage(:,:,1).*uint8(1-imdilate(elastinLayers_3,ones(3)))) ;
+outputLayersPoints(:,:,1)       = uint8(175*imdilate(elastinLayers,ones(3))) +...
+                                (currImage(:,:,1).*uint8(1-imdilate(elastinLayers,ones(3)))) ;
 
-outputLayersPoints(:,:,2)       = uint8(240*imdilate(elastinEndPoints_3_L>0,ones(14))) +...
-                                (currImage(:,:,2).*uint8(1-imdilate(elastinEndPoints_3_L>0,ones(14)))) ;
+outputLayersPoints(:,:,2)       = uint8(240*imdilate(elastinEndPoints>0,ones(14))) +...
+                                (currImage(:,:,2).*uint8(1-imdilate(elastinEndPoints>0,ones(14)))) ;
 for k=1:numEndPoints
-    outputLayersPoints          = insertText(outputLayersPoints,[ elastinEndPoints_3_P(k).Centroid(1),elastinEndPoints_3_P(k).Centroid(2)],num2str(k),FontSize=50,BoxOpacity=0.2,TextColor='black',TextBoxColor='white');
+    outputLayersPoints          = insertText(outputLayersPoints,[ elastinEndPoints_P(k).Centroid(1),elastinEndPoints_P(k).Centroid(2)],num2str(k),FontSize=50,BoxOpacity=0.2,TextColor='black',TextBoxColor='white');
 end
 
 outputLayersPoints              = insertText(outputLayersPoints,[ 1,1],strcat('Number of points=',num2str(numEndPoints)),FontSize=200,BoxOpacity=0.2,TextColor='black',TextBoxColor='white');
