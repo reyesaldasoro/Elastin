f60b9e00116af2d00a590eaea6790f91c8aff07f
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2026-02-26 11:23:03 +0000

    Update detectElastinLayers.m
    Move the visual outputs to a separate function

--- CODE/detectElastinLayers.m
+++ CODE/detectElastinLayers.m
@@ -1,4 +1,4 @@
-function [elastinLayers,OutputLayers,outputLayersPoints,numEndPoints] =detectElastinLayers(currImage)
+function [elastinLayers,elastinEndPoints_3_L] =detectElastinLayers(currImage)
 
 
 [redInverted,watershedIntensity]        = detectLumenBackground(currImage);
@@ -8,23 +8,6 @@ elastinLayers                           = regionGrowingElastin(watershedIntensit
 
 [elastinEndPoints_3_L,elastinLayers_3,numEndPoints]  = postprocessElastinLayers(elastinLayers,redInverted);
 
-elastinEndPoints_3_P            = regionprops(elastinEndPoints_3_L,"Centroid");
-
-OutputLayers                  = currImage;
-outputLayersPoints            = currImage;
-OutputLayers(:,:,2)           = uint8(255*imdilate(elastinLayers,ones(3))) +...
-                                (currImage(:,:,2).*uint8(1-imdilate(elastinLayers,ones(3)))) ;
-
-outputLayersPoints(:,:,1)           = uint8(175*imdilate(elastinLayers_3,ones(3))) +...
-                                (currImage(:,:,1).*uint8(1-imdilate(elastinLayers_3,ones(3)))) ;
-
-outputLayersPoints(:,:,2)           = uint8(240*imdilate(elastinEndPoints_3_L>0,ones(14))) +...
-                                (currImage(:,:,2).*uint8(1-imdilate(elastinEndPoints_3_L>0,ones(14)))) ;
-for k=1:numEndPoints
-    outputLayersPoints = insertText(outputLayersPoints,[ elastinEndPoints_3_P(k).Centroid(1),elastinEndPoints_3_P(k).Centroid(2)],num2str(k),FontSize=50,BoxOpacity=0.2,TextColor='black',TextBoxColor='white');
-end
-
-outputLayersPoints = insertText(outputLayersPoints,[ 1,1],strcat('Number of points=',num2str(numEndPoints)),FontSize=200,BoxOpacity=0.2,TextColor='black',TextBoxColor='white');
 
 
 
